827dbc7e16bb637d528b4c952870ed7853a1fa2d
Cesareuh <mathieu.graff498@gmail.com>
2024-11-06 15:11:54 +0100

    squelette pour le projet

---
+++ code/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sae/nrv',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4b14c3d9bed2754a840063f1d9dcc48a5e3161b7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sae/nrv' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4b14c3d9bed2754a840063f1d9dcc48a5e3161b7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
