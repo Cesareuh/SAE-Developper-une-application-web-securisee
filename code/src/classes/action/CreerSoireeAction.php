ebefad08a92dd13ceea7dbd6e545de33a4290b35
firevaam <firevaam@gmail.com>
2024-11-07 08:45:27 +0100

    (ne pas pull c'est un test)

--- code/src/classes/action/CreerSoireeAction.php
+++ code/src/classes/action/CreerSoireeAction.php
@@ -4,9 +4,25 @@ namespace iutnc\nrv\action;
 
 class CreerSoireeAction extends Action
 {
+    public function execute(): string{
+        if($this->http_method === 'GET'){
+			return <<<END
+        <form id="form" method="post" action="?action=add-soiree">
+        <label>Nom playlist:
+        <input type="text" name="nom_soiree" placeholder="nom de la soirée"/>
+        </label>
+        </form>
+        <button type="submit" form="form" value="Valider">Valider</button>
+        END;
+        }
+        else{
+            $repo = Repository::getInstance();
+            $_SESSION['soiree'] = new Soiree(filter_var($_POST['nom_soiree'], FILTER_SANITIZE_SPECIAL_CHARS));
 
-    public function execute(): string
-    {
-        // TODO: Implement execute() method.
+            $_SESSION['soiree'] = $repo->saveEmptyPlaylist($_SESSION['soiree'], AuthnProvider::getSignedInUser()['id']);
+            
+            // $render = (new AudioListRenderer($_SESSION['playlist']))->render();
+            return "<div>Soirée créée</div>";
+        }
     }
 }
