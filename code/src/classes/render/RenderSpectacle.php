3db1bb0f19bc674ed0d3c01aa8d1b05a7e8e6d58
Cesareuh <mathieu.graff498@gmail.com>
2024-11-07 15:20:30 +0100

    je sais plus ce que j'ai fait...

--- code/src/classes/render/RenderSpectacle.php
+++ code/src/classes/render/RenderSpectacle.php
@@ -15,18 +15,24 @@ class RenderSpectacle extends Renderer{
 		$res = "";
 		switch($selector){
 		case 1:
-			$res = $res ."<div>
-				<img ".$this->spec->photo_artiste." alt=Photo de ".$this->spec->artiste."/>
-				<h1 id=artiste>".$this->spec->artiste."</h1>
-				<h2 id=titre>".$this->spec->titre."</h2>
-				<p id=style>".$this->spec->style."</p>
-				<p id=duree>".$this->spec->duree."</p>
-				<p id=desc>".$this->spec->description."</p>
-				 <iframe width=\"420\" height=\"315\"
-				src=\"https://www.youtube.com/embed/tgbNymZ7vqY\"></iframe>
+			$res = $res ."<div class=simple>
+				<h1 class=artiste>".$this->spec->artiste."</h1>
+				<h2 class=titre>".$this->spec->titre."</h2>
+				<p class=style>".$this->spec->style."</p>
+				<p class=duree>".$this->spec->duree."</p>
 				</div>";
 			break;
 		case 2:
+			$res = $res ."<div class=complexe>
+				<img src=\"".$this->spec->photo_artiste."\" alt=\"Photo de ".$this->spec->artiste."\"/>
+				<h1 class=artiste>".$this->spec->artiste."</h1>
+				<h2 class=titre>".$this->spec->titre."</h2>
+				<p class=style>".$this->spec->style."</p>
+				<p class=duree>".$this->spec->duree."</p>
+				<p class=desc>".$this->spec->description."</p>
+				 <iframe width=\"420\" height=\"315\"
+				src=\"".$this->spec->video."\"></iframe>
+				</div>";
 			break;
 		}
 		return $res;
