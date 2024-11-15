d9bf20b7e5fa1d3a69df342955ea24b18960b5a9
Cesareuh <mathieu.graff498@gmail.com>
2024-11-15 11:48:35 +0100

    suppression vérif vidéo

--- code/src/classes/render/RenderSpectacle.php
+++ code/src/classes/render/RenderSpectacle.php
@@ -111,13 +111,11 @@ class RenderSpectacle extends Renderer
                         <p class='desc'>".$this->spec->description."</p>
                         <hr>";
     
-                // Affichage de la vidéo si elle existe
-                if (!empty($this->spec->video)) {
-                    $res .= "<iframe width='560' height='315' src='".$this->spec->video."' 
-                                title='YouTube video player' frameborder='0' 
-                                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' 
-                                referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>";
-                }
+                // Affichage de la vidéo
+				$res .= "<iframe width='560' height='315' src='".$this->spec->video."' 
+					title='YouTube video player' frameborder='0' 
+					allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' 
+					referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>";
     
                 $res .= "</div>
                     <h1>Soirées incluant ce spectacle</h1>
